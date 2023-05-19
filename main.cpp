48b7d13dd9f246175c0ff106c565a37367d3da99
Poogal0 <golubchikov_2002@mail.ru>
2023-05-19 19:33:49 +0400

    v4 - added QLabel into app

--- main.cpp
+++ main.cpp
@@ -3,12 +3,18 @@
 class MainWindow : public QMainWindow {
     public:
         MainWindow();
-
+    private:
+        QLabel *textLabel;
 };
 
 MainWindow::MainWindow() {
     resize(900,500);
     setWindowTitle("Main win v6.1");
+    textLabel = new QLabel(this);
+    textLabel->setText("Text QLabel");
+    QFont fontLabel("Time", 32, QFont::Bold);
+    textLabel->setFont(fontLabel);
+    textLabel->setGeometry(0, 0, 800, 300);
 }
 
 int main (int argc, char *argv[]) {
